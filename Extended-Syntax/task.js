233d768defa11565fa24f79d8ea5de7d0541d915
Susanna Alieva <susanna_susanna_00@mail.ru>
2019-05-23 18:23:03 +0300

    Firs task is solved

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -13,7 +13,49 @@ function calculateQuadraticEquation(){
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
     //return x;
+    "use strict";
+
+    let D = Math.pow(b, 2) - 4 * a * c;
+    if (D < 0 ) 
+       console.log("Корней нет");
+    else if (D == 0) {
+      let x = -b / 2 * a;
+      return x;
+    } else if (D > 0) {
+      let x1 = (-b + Math.sqrt(D)) / 2 * a;
+      let x2 = (-b - Math.sqrt(D)) / 2 * a;
+      return [x1, x2];
+    }
 }
+getResult();
+
+
+/* Подскажите, пожалуйста, как мне сделать 
+эту же функцию со switch case. 
+Совершенно не могу разобраться, как пользоваться этой консрукцией(((
+  Что я здесь неправильно пишу?? */
+function giveResult(a, b, c) {
+  let D = Math.pow(b, 2) - 4 * a * c;
+  switch(D){
+  case (D < 0):
+    alert("Корней нет");
+    break;
+  case 0:
+    let x = -b / 2 * a;
+    alert("x = " + x);
+    break;    
+  case D > 0 :
+    let x1 = (-b + Math.sqrt(D)) / 2 * a;
+    let x2 = (-b - Math.sqrt(D)) / 2 * a;
+    let roots = [x1, x2];
+    alert("Корни уравнения: " + roots);
+  default:
+    alert("Я такого не умею");  
+  }
+}
+giveResult();
+
+
 
 function calculateDrinkTask(){
     let name = window.personName.value;
